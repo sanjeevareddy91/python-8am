27baf860454cb62547e8f3d6d4a275146e6e7401
sanjeevareddy91 <gsanjeevreddy91@gmail.com>
2022-12-09 08:36:44 +0530

    First file push to the git

---
+++ Tuples_cls.py
@@ -0,0 +1,46 @@
+# Tuples -- Sequence of elements seperated with comma(,) declared inside ( ).
+
+a = (43,87,'python',67.8,13,[45,46,47])
+
+# print(a)
+
+# print(type(a))
+
+b = 43,87,'python',67.8,13,[45,46,47]
+
+# print(b)
+
+# Accessing the elements inside the sequence..(indexing)
+
+# Indexing starts from 0 ..
+
+print(a[0])
+
+# print(a[1:5])
+
+# print(a[0:6:2])
+
+# Negative indexing will start from -1..
+
+# print(a[-4])
+
+# print(a[::-1])
+
+# Tuples are immutables .. 
+
+# Once a tuple is declared we cannot make changes to the tuple..
+
+# a[0] = 54
+
+# only 2 methods inside the tuples..
+
+# index : It will return the index value of the element inside the sequence.
+# count : It will return how many times a particular element is repeated inside the sequence.
+
+
+# a = (43,87,'python',67.8,13,[45,46,47])
+
+
+# print(a.index(13))
+
+# print(a.count(43))
